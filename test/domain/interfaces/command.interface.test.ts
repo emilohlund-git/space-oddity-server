7942e7df6ea64ddb57a5798b27ba115e2b6d0542
Emilolund <emil@emilohlund.dev>
2023-07-16 14:15:22 +0200

    🔄 Covering abstract command class with unit tests

---
+++ test/domain/interfaces/command.interface.test.ts
@@ -0,0 +1,37 @@
+import { createPayloadValidationRules, validatePayload } from '../../../src/application/utils/payload.validator';
+import { Command } from '../../../src/domain/interfaces/command.interface';
+
+jest.mock('../../../src/application/utils/payload.validator');
+
+class MockCommand extends Command {
+  constructor(private readonly payload: any) {
+    super(payload);
+  }
+
+  execute(): void { }
+}
+
+describe('Command', () => {
+  const payload = { /* mock payload */ };
+  const payloadValidationRules = { /* mock validation rules */ };
+
+  beforeEach(() => {
+    (createPayloadValidationRules as jest.Mock).mockReturnValue(payloadValidationRules);
+    (validatePayload as jest.Mock).mockImplementation(() => {
+      // Validation logic
+    });
+  });
+
+  it('should validate the payload during construction', () => {
+    new MockCommand(payload);
+
+    expect(createPayloadValidationRules).toHaveBeenCalledWith(payload);
+    expect(validatePayload).toHaveBeenCalledWith(payload, payloadValidationRules);
+  });
+
+  it('should have an execute method', () => {
+    const command = new MockCommand(payload);
+
+    expect(command.execute).toBeDefined();
+  });
+});
