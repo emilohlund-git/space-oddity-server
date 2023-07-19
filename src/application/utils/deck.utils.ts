397fb3defaebc8df36c5eca77b0834f56408e1d8
Emilolund <emil@emilohlund.dev>
2023-07-19 18:04:36 +0200

    🐛 Fixed creating duplicates of special cards

--- src/application/utils/deck.utils.ts
+++ src/application/utils/deck.utils.ts
@@ -7,11 +7,11 @@ export const createCard = (value: number): Card => {
   if (value === 21 || value === 22) {
     return new BlackHoleCard(value);
   } else if (value === 23) {
-    return new TwistedCard(value, SpecialEffect.SwitchLight);
+    return new TwistedCard(value, SpecialEffect.SwitchLight, 'Play this card to change the light color.');
   } else if (value === 24) {
-    return new TwistedCard(value, SpecialEffect.SwapHand);
+    return new TwistedCard(value, SpecialEffect.SwapHand, 'Play this card and choose an opponent to swap hands with.');
   } else if (value === 25) {
-    return new TwistedCard(value, SpecialEffect.SneakAPeak);
+    return new TwistedCard(value, SpecialEffect.SneakAPeak, 'Play this card and choose an opponent to spy on and pick a card from their hand.');
   } else {
     return new Card(value);
   }
@@ -24,7 +24,11 @@ export const getShuffledDeck = (): Deck => {
     const card = createCard(value);
     cards.push(card);
 
-    if (value !== 19 || card.getType() === CardType.Regular) {
+    if (card.getType() !== CardType.Regular) {
+      continue;
+    }
+
+    if (value !== 19) {
       cards.push(card.clone());
     }
   }
--- src/domain/entities/TwistedCard.ts
+++ src/domain/entities/TwistedCard.ts
@@ -9,13 +9,20 @@ export enum SpecialEffect {
 class TwistedCard extends Card {
   private specialEffect: SpecialEffect;
 
-  constructor(value: number, specialEffect: SpecialEffect) {
+  private description?: string;
+
+  constructor(value: number, specialEffect: SpecialEffect, description?: string) {
     super(value, CardType.Twisted);
     this.specialEffect = specialEffect;
+    this.description = description;
   }
 
   public getSpecialEffect(): SpecialEffect {
     return this.specialEffect;
+  }
+
+  public getDescription(): string | undefined {
+    return this.description;
   }
 
   public play(): void {
